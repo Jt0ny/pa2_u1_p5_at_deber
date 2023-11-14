6984a49c70d9478871249756717e74d628eb6b2e
Anthony T <jtonytipan@gmail.com>
2023-11-14 15:03:44 -0500

    Tarea

---
+++ src/main/java/com/uce/edu/Pa2U1P5AtDeberApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5AtDeberApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5AtDeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Tarea");
+	}
+
+}
